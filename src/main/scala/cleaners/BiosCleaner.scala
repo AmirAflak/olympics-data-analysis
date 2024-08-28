0df80698a0b1009bf544bda7f7aa1d91d9da7ac3
amiraflak <mehrdad.aflakparast@gmail.com>
2024-08-29 01:54:55 +0330

    refactor: BiosCleaner to use OOP principles

--- src/main/scala/cleaners/BiosCleaner.scala
+++ src/main/scala/cleaners/BiosCleaner.scala
@@ -1,41 +1,51 @@
+package cleaners
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.DateTime
 import java.util.Locale
 import java.sql.Date
 
+class BiosCleaner(sparkSession: SparkSession) {
 
-object BiosCleaner {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
-      .appName("BiosCleaner")
-      .master("local[2]") 
-      .config("spark.log.level", "ERROR")
-      .getOrCreate()
+  def cleanBios(df: Dataset[Row]): Dataset[Row] = {
+    val dfWithReplacedName = replaceName(df)
+    val dfWithMeasurements = extractMeasurements(dfWithReplacedName)
+    val dfWithCleanedMeasurements = cleanMeasurements(dfWithMeasurements)
+    val dfWithParsedDates = parseDates(dfWithCleanedMeasurements)
+    val dfWithDateTime = convertToDateTime(dfWithParsedDates)
+    val dfWithTimestamp = convertToTimestamp(dfWithDateTime)
+    val dfWithLocations = extractLocations(dfWithTimestamp)
+    val dfCleaned = selectColumns(dfWithLocations)
 
-    val bios = spark.read.format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load("/home/mehrdad/olympic-data-analysis/datasets/bios.csv")
+    dfCleaned
+  }
 
-    val dfWithReplacedName = bios.withColumn("name", regexp_replace(col("Used name"), "•", " "))
+  private def replaceName(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("name", regexp_replace(col("Used name"), "•", " "))
+  }
 
-    val dfWithMeasurements = dfWithReplacedName
-      .withColumn("height_cm", split(col("Measurements"), "/").getItem(0))
+  private def extractMeasurements(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("height_cm", split(col("Measurements"), "/").getItem(0))
       .withColumn("weight_kg", split(col("Measurements"), "/").getItem(1))
+  }
 
-    val dfWithCleanedMeasurements = dfWithMeasurements
-      .withColumn("height_cm", regexp_replace(col("height_cm"), " cm", "").cast("double"))
+  private def cleanMeasurements(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("height_cm", regexp_replace(col("height_cm"), " cm", "").cast("double"))
       .withColumn("weight_kg", regexp_replace(col("weight_kg"), " kg", "").cast("double"))
+  }
 
+  private def parseDates(df: Dataset[Row]): Dataset[Row] = {
     val datePattern = "(\\d{1,2} [A-Za-z]+ \\d{4})"
-
-    val dfWithParsedDates = dfWithCleanedMeasurements
-      .withColumn("born_date", regexp_extract(col("Born"), datePattern, 0))
+    df.withColumn("born_date", regexp_extract(col("Born"), datePattern, 0))
       .withColumn("died_date", regexp_extract(col("Died"), datePattern, 0))
+  }
 
+  private def convertToDateTime(df: Dataset[Row]): Dataset[Row] = {
     val parseDate = udf((date: String) => {
       if (date == null) {
         null
@@ -46,28 +56,25 @@ object BiosCleaner {
       }
     })
 
-
-    val dfWithDateTime = dfWithParsedDates
-      .withColumn("born_date", parseDate(col("born_date")))
+    df.withColumn("born_date", parseDate(col("born_date")))
       .withColumn("died_date", parseDate(col("died_date")))
+  }
 
-    val dfWithTimestamp = dfWithDateTime
-      .withColumn("born_date", unix_timestamp(col("born_date"), "yyyy-MM-dd").cast("timestamp"))
+  private def convertToTimestamp(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("born_date", unix_timestamp(col("born_date"), "yyyy-MM-dd").cast("timestamp"))
       .withColumn("died_date", unix_timestamp(col("died_date"), "yyyy-MM-dd").cast("timestamp"))
+  }
 
-    val locationPattern = """in ([\w\p{L}\s()-]+), ([\w\p{L}\s-]+) \((\w+)\)"""
-
-        val dfWithLocations = dfWithTimestamp
-      .withColumn("born_city", regexp_extract(col("Born"), locationPattern, 1))
+  private def extractLocations(df: Dataset[Row]): Dataset[Row] = {
+    val locationPattern = """in ([\w\p{L}\s()-]+), ([\w\p{L}\s-]+) $$(\w+)$$"""
+    df.withColumn("born_city", regexp_extract(col("Born"), locationPattern, 1))
       .withColumn("born_region", regexp_extract(col("Born"), locationPattern, 2))
       .withColumn("born_country", regexp_extract(col("Born"), locationPattern, 3))
+  }
 
+  private def selectColumns(df: Dataset[Row]): Dataset[Row] = {
     val columnsToKeep = Seq("athlete_id", "name", "born_date", "born_city", "born_region", "born_country", "NOC", "height_cm", "weight_kg", "died_date")
-
-    val dfCleaned = dfWithLocations.select(columnsToKeep.map(col): _*)
-    
-    dfCleaned.show(5)
-
-    spark.stop()
+    df.select(columnsToKeep.map(col): _*)
   }
 }
+
