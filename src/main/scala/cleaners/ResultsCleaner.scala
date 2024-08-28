02c5dbee4b69cf47f5462c8090f4b12ebcfd40e3
amiraflak <mehrdad.aflakparast@gmail.com>
2024-08-29 01:59:55 +0330

    refactor: ResultsCleaner to improve code organization

--- src/main/scala/cleaners/ResultsCleaner.scala
+++ src/main/scala/cleaners/ResultsCleaner.scala
@@ -1,51 +1,64 @@
+package cleaners
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.Row
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.DateTime
 import java.util.Locale
 import java.sql.Date
 
+class ResultsCleaner(sparkSession: SparkSession) {
 
-object ResultsCleaner {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder
-      .appName("BiosCleaner")
-      .master("local[2]") 
-      .config("spark.log.level", "ERROR")
-      .getOrCreate()
+  def cleanResults(df: Dataset[Row]): Dataset[Row] = {
+    val dfWithPlaceAndTied = extractPlaceAndTied(df)
+    val dfWithMedalValue = extractMedalValue(dfWithPlaceAndTied)
+    val dfWithYearAndType = extractYearAndType(dfWithMedalValue)
+    val dfWithYearAsInt = convertYearToInt(dfWithYearAndType)
+    val dfSorted = sortResults(dfWithYearAsInt)
+    val dfFiltered = filterColumns(dfSorted)
+    val dfCleaned = renameColumns(dfFiltered)
 
-    val results = spark.read.format("csv")
-      .option("header", "true")
-     .option("inferSchema", "true")
-        .load("/home/mehrdad/olympic-data-analysis/datasets/results.csv")
-    
-    val dfWithPlaceAndTied = results
-    .withColumn("place", regexp_extract(col("Pos"), "(\\d+)", 1).cast("int"))
-    .withColumn("tied", col("Pos").contains("="))
-    
-        val dfWithMedalValue = dfWithPlaceAndTied.withColumn("medal_value", when(col("Medal") === "Bronze", 3)
-    .when(col("Medal") === "Silver", 2)
-    .when(col("Medal") === "Gold", 1)
-    .otherwise(null))
+    dfCleaned
+  }
 
-    val dfWithYearAndType = dfWithMedalValue.withColumns(
-    Map(
+  private def extractPlaceAndTied(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("place", regexp_extract(col("Pos"), "(\\d+)", 1).cast("int"))
+      .withColumn("tied", col("Pos").contains("="))
+  }
+
+  private def extractMedalValue(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("medal_value", when(col("Medal") === "Bronze", 3)
+      .when(col("Medal") === "Silver", 2)
+      .when(col("Medal") === "Gold", 1)
+      .otherwise(null))
+  }
+
+  private def extractYearAndType(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumns(
+      Map(
         "year" -> regexp_extract(col("Games"), "(\\d{4}) (Summer|Winter)", 1).cast("int"),
         "type" -> regexp_extract(col("Games"), "(\\d{4}) (Summer|Winter)", 2)
+      )
     )
-    )
+  }
 
-     val dfWithYearAsInt = dfWithYearAndType.withColumn("year", col("year").cast("int"))
+  private def convertYearToInt(df: Dataset[Row]): Dataset[Row] = {
+    df.withColumn("year", col("year").cast("int"))
+  }
 
-     val dfSorted = dfWithYearAsInt.sort(col("year").desc)
+  private def sortResults(df: Dataset[Row]): Dataset[Row] = {
+    df.sort(col("year").desc)
+  }
 
-     val columnsToKeep = Seq("year", "type", "Discipline", "Event", "As", "athlete_id", "NOC", "Team", "place", "tied", "Medal")
-     val dfFiltered = dfSorted.select(columnsToKeep.map(col): _*)
+  private def filterColumns(df: Dataset[Row]): Dataset[Row] = {
+    val columnsToKeep = Seq("year", "type", "Discipline", "Event", "As", "athlete_id", "NOC", "Team", "place", "tied", "Medal")
+    df.select(columnsToKeep.map(col): _*)
+  }
 
-     val dfCleaned = dfFiltered.columns.foldLeft(dfFiltered)((acc, c) => acc.withColumnRenamed(c, c.toLowerCase.replace(" ", "_")))
-    
-    dfCleaned.show(5)
-    spark.stop()
+  private def renameColumns(df: Dataset[Row]): Dataset[Row] = {
+    df.columns.foldLeft(df)((acc, c) => acc.withColumnRenamed(c, c.toLowerCase.replace(" ", "_")))
   }
 }
